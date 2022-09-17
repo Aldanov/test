6cf7b53868983d89596fd2a6aced4f778eef6fd7
Aldanov <aldanov@ukr.net>
2022-09-17 19:13:51 +0300

    start-settings

---
+++ index.js
@@ -0,0 +1,24 @@
+let a = 5;
+let b = 10;
+
+console.log(a + b);
+
+// import './App.css';
+// import HelloWorld from "./components/HelloWorld";
+
+// function App() {
+//   return (
+//     <div className="App">
+      
+//       <HelloWorld />
+//     </div>
+//   );
+// }
+
+// const HelloWorld = () => {
+// 	return (
+// 		<h1>Hello World</h1>
+// 	);
+// }
+
+// export default HelloWorld;
